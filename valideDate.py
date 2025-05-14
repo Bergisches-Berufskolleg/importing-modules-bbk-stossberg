5531a79e8adf47b7d9064975adfd9e23da493733
bbk-stossberg <24-hbfi1.stossberg@bbk.schule>
2025-05-14 12:34:50 +0200

    Ich bin fertig

--- valideDate.py
+++ valideDate.py
@@ -1,8 +1,18 @@
 from leapYear import isLeapYear
 
 def validateDate(day, month, year):
-    # ...
-    return True
+    if(month in [4,6,9,11] and day > 30 ):
+        return False
+    elif(isLeapYear(year) == True and month == 2 and day > 29 ):
+        return False
+    elif(isLeapYear(year)== False and month == 2 and day > 28 ):
+        return False
+    elif(day < 1 ) or (day > 31):
+        return False
+    elif(month < 1) or (month > 12):
+        return False
+    else:
+        return True
 
 def main():
     day   = int(input("Gib ein Tag ein: "))
